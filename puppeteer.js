b646d0d2eefb68d206ae811a5bdaab92dfb0d71a
Duo L <ld3577@gmail.com>
2019-02-23 11:58:44 +0900

    first version

---
+++ puppeteer.js
@@ -0,0 +1,97 @@
+const puppeteer = require("puppeteer");
+class TweetWiper {
+  async run() {
+    this.browser = await puppeteer.launch({ headless: false });
+    this.page = await this.browser.newPage();
+    this.count = 0;
+    const { page } = this;
+    const navigationPromise = page.waitForNavigation();
+
+    await page.goto("https://twitter.com/r_18/media");
+
+    await page.setViewport({ width: 1536, height: 731 });
+    await page.waitForSelector(
+      "#signin-dropdown > .signin-dialog-body > .LoginForm > .LoginForm-username > .text-input"
+    );
+    // await page.waitForSelector(
+    //   ".AppContainer > .AppContent-main > .Grid > .Grid-cell:nth-child(2) > .Grid"
+    // );
+    // await page.click(
+    //   ".AppContainer > .AppContent-main > .Grid > .Grid-cell:nth-child(2) > .Grid"
+    // );
+
+    await page.waitForSelector(
+      ".pull-right > #session > .dropdown > #signin-link > .emphasize"
+    );
+    // await page.click(
+    //   ".pull-right > #session > .dropdown > #signin-link > .emphasize"
+    // );
+    await page.waitFor(1000);
+    await page.type(
+      "#signin-dropdown > .signin-dialog-body > .LoginForm > .LoginForm-username > .text-input",
+      "YOUR TWITTER ACCOUNT"
+    );
+    await page.type(
+      "#signin-dropdown > .signin-dialog-body > .LoginForm > .LoginForm-password > .text-input",
+      "YOUR PASSWORD"
+    );
+
+    await page.waitForSelector(
+      ".dropdown > #signin-dropdown > .signin-dialog-body > .LoginForm > .EdgeButton"
+    );
+    await page.click(
+      ".dropdown > #signin-dropdown > .signin-dialog-body > .LoginForm > .EdgeButton"
+    );
+
+    await navigationPromise;
+
+    console.log("Wiping started....");
+
+    for (let i = 0; i <= 100; i++) {
+      await page.waitFor(1000);
+      // Page needs to be reloaded every 20 deletions, otherwise the page will no more display tweets.
+      console.log(`Started: ${i}`)
+      if (i % 19 === 1) await page.reload();
+      try {
+        await this.wiper();
+      } catch (e) {
+        console.error(`${e}`)
+        //FIXME: Every 115 attempts the function stops working
+        await page.reload();
+        console.log("Page reloaded")
+        await page.waitFor(2000);
+      }
+    }
+
+    await this.browser.close();
+    console.log("Wipe completed!");
+  }
+
+  async wiper() {
+    const { page } = this;
+    await page.waitForSelector(".stream > ol.stream-items");
+    await page.waitForSelector(
+      ".stream ol.stream-items li.js-stream-item .content .dropdown .dropdown-toggle"
+    );
+    await page.click(
+      ".stream ol.stream-items li.js-stream-item .content .dropdown .dropdown-toggle"
+    );
+
+    await page.waitForSelector(
+      ".stream > ol.stream-items > li.js-stream-item .content .dropdown li.js-actionDelete > .dropdown-link"
+    );
+    await page.click(
+      ".stream > ol.stream-items > li.js-stream-item .content .dropdown li.js-actionDelete > .dropdown-link"
+    );
+
+    await page.waitForSelector(
+      "#delete-tweet-dialog > #delete-tweet-dialog-dialog > .modal-content > .modal-footer > .EdgeButton--danger"
+    );
+    await page.click(
+      "#delete-tweet-dialog > #delete-tweet-dialog-dialog > .modal-content > .modal-footer > .EdgeButton--danger"
+    );
+    console.log(`Succeeded: ${this.count++}`);
+  }
+}
+
+new TweetWiper().run();
